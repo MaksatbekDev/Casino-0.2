c325a5e9ddca298696f05d96168d00de0207862e
Emir Bolotbekov <131597014+gerrniger@users.noreply.github.com>
2023-08-27 22:16:39 +0600

    MenuStructure.java
    Upate 27.08.23

--- MenuStructure.java
+++ MenuStructure.java
@@ -3,18 +3,28 @@ import java.util.Scanner;
 public class MenuStructure {
     Scanner scanner = new Scanner(System.in);
     public void mainManu() {
-        Local.mainLogo();
+        Local.mainMenuDecor();
         System.out.println("1. Start\n2. Options\n3. Exit");
-        while (true) {
-            try {
-                System.out.print("• Choose an option: ");
-                int chooseOption = scanner.nextInt();
-                if (chooseOption < 1 || chooseOption > 3) Local.notify(1);
-                else break;
-            } catch (java.util.InputMismatchException e) {
-                Local.notify(1);
-                scanner.nextLine();
+        switch (Local.tryBlock("Choose an action", 1, 3)) {
+            case 1 -> selectGame();
+            case 2 -> System.out.println("Options");
+            case 3 -> {
+                Local.notify(6);
+                System.exit(0);
             }
+            default -> Local.notify(3);
+        }
+    }
+    public void selectGame() {
+        Local.selectGameDecor();
+        System.out.println("1. Slot machine \uD83C\uDFB0\n2. Roulette\n3. Black Jack\n4. Poker\n5. Back");
+        switch (Local.tryBlock("Choose an action", 1, 5)) {
+            case 1 -> new SlotMachine().initGame();
+            case 2 -> System.out.println("Roulette");
+            case 3 -> System.out.println("Black Jack");
+            case 4 -> System.out.println("Poker");
+            case 5 -> mainManu();
+            default -> Local.notify(3);
         }
     }
 }
